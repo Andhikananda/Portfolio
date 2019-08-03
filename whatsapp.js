a5d653adb6332df42bb53382ebc01d46329ad725
Andhikananda <akun.andhika.nanda@gmail.com>
2019-08-04 05:33:39 +0700

    Update whatsapp.js

--- whatsapp.js
+++ whatsapp.js
@@ -97,7 +97,7 @@ function kirimWA(id) {
             var parameter = url_wa + '?phone=' + kode_area + nomor_whatsapp + '&text=' +
                 pesan_salam + ' ' + nama_admin + '.. ' +
                 'saya *' + nama + '*, ingin *' + judul + '*.' +
-                '%0A%0AðŸ’¬ ' + pesan + '%0A%0A' +
+                '💬 ' + pesan + '%0A%0A' +
                 'E-mail: ' + email + '%0A' +
                 'via ' + location.href;
 
